d420c2b3e2de0b0455273c4b809d1aac276092ab
elizadelacin <elizade1428@gmail.com>
2025-06-05 20:54:58 +0400

    Layihəni yeni path ilə yenilə

--- second_page/settings.py
+++ second_page/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 from django.conf.global_settings import LOCALE_PATHS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,6 +31,8 @@ SECRET_KEY = 'django-insecure-yh(k67l-bpp&!12wkihe!$9r9c7(g(b=%sc)x=4h^9-(^#c%&o
 DEBUG = True
 
 ALLOWED_HOSTS = ["13.51.121.199"]
+
+print(os.getenv('DB_NAME'))
 
 
 # Application definition
@@ -89,8 +93,12 @@ WSGI_APPLICATION = 'second_page.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
